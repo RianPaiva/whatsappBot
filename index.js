4f3c0513a96f5f916b0219f2c965e533468ba911
Rian Paiva <rianpaiva0@gmail.com>
2023-02-08 17:22:19 -0300

    menu protype

--- index.js
+++ index.js
@@ -14,32 +14,37 @@ venom
 
 function start(client) {
   client.onMessage((message) => {
-    if (message.body === 'Hi' && message.isGroupMsg === false) {
-      client
-        .sendText(message.from, 'Olá, seja bem vindo ao PaivaBot')        
-        .then((result) => {
+    
+    switch (String(message.body).toUpperCase()) {
+        case 'HI' :
 
-          client.sendText(message.from, '====== MENU ====== \n \n 1. OPÇÃO \n \n 2. OPÇÃO \n \n 3. OPÇÃO') 
-
-
-
-          console.log('Result: ', result); //return object success
-        })
-        .catch((erro) => {
-          console.error('Error when sending: ', erro); //return object error
-        });
-    }else{
-      if (message.body !== 'Hi' && message.isGroupMsg === false) {
         client
-          .sendText(message.from, 'Algo de errado não está certo')        
-          .then((result) => {
-            console.log('Result: ', result); //return object success
-          })
-          .catch((erro) => {
-            console.error('Error when sending: ', erro); //return object error
-          });
-      }
+        .sendText(message.from, '====== MENU ====== \n \n 1. OPÇÃO \n \n 2. OPÇÃO \n \n 3. OPÇÃO')
+            break;
+    
+        default: 
+            if(message.body === '1' ){
+                client
+                    .sendText(message.from, 'Bem Vindo ao menu 1')
+            }else if (message.body === '2'){
+                client
+                    .sendText(message.from, 'Bem Vindo ao menu 2')
+            }else if(message.body === '3'){
+                client
+                    .sendText(message.from, 'Bem vindo ao menu 3')
+            }
+
+            break;
     }
+
+
   });
 }
+
+
+
+
+
+
+
 
